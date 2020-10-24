7ad1dd0a4820ea2c7e9ae950fd67dd58650d8c6f
Matt Patek <patekmatt@protonmail.com>
2020-10-24 11:30:27 -0500

    revise

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 
+const addMeme = require('./lib/getMeme')
 const getMeme = require('./lib/getMeme')
+const removeMeme = require('./lib/removeMeme')
+const updateMeme = require('./lib/updateMeme')
 
 const app = express()
 const port = 3000
 
-app.route('/meme')
+app.route('/meme/:id')
+  .delete(removeMeme)
   .get(getMeme)
+  .post(addMeme)
+  .put(updateMeme)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
