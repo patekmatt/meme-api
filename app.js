00401eb68d2a967949a60e7129037f88db621735
patekmatt <patekmatt@protonmail.com>
2020-10-25 11:06:16 -0500

    Random (#3)
    * random meme works
    
    * random meme
    
    * allow bigger files to be uploaded
    
    * move dotenv stuff
    
    * simplify add meme

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express')
+require('dotenv').config()
 
 const {
-  addMeme, getMeme, removeMeme, updateMeme
+  addMeme, getMeme, randomMeme, removeMeme, updateMeme
 } = require('./lib/service')
 
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 const port = 3000
 
 app.post('/meme', addMeme)
+app.get('/meme/random', randomMeme)
 app.route('/meme/:id')
   .delete(removeMeme)
   .get(getMeme)
   .put(updateMeme)
-// app.get('/meme/random', randomMeme)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
