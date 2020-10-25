88d46d1087bdc80371ffed0391bb8ff7df279817
patekmatt <patekmatt@protonmail.com>
2020-10-25 12:00:37 -0500

    Joi Validation (#4)
    * random meme works
    
    * random meme
    
    * allow bigger files to be uploaded
    
    * move dotenv stuff
    
    * simplify add meme
    
    * add schemas
    
    * joi stuff
    
    * joi validation"
    
    * fixes

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express')
+const { validate, ValidationError } = require('express-validation')
 require('dotenv').config()
 
 const {
   addMeme, getMeme, randomMeme, removeMeme, updateMeme
 } = require('./lib/service')
+const schemas = require('./lib/schemas')
 
 const app = express()
 app.use(express.json({ limit: '50mb' }))
+
 const port = 3000
 
-app.post('/meme', addMeme)
+app.post('/meme', validate(schemas.addMeme), addMeme)
 app.get('/meme/random', randomMeme)
 app.route('/meme/:id')
-  .delete(removeMeme)
-  .get(getMeme)
-  .put(updateMeme)
+  .delete(validate(schemas.getMeme), removeMeme)
+  .get(validate(schemas.getMeme), getMeme)
+  .put(validate(schemas.updateMeme), updateMeme)
+
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err)
+  }
+  return res.status(500).json(err)
+})
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
