6fa1bff3956564c756f1f9dc7a606efbd09a8b06
Matt Patek <patekmatt@protonmail.com>
2020-10-27 16:54:42 -0500

    get all memes

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -10,11 +10,20 @@ const MEME_2_ID = '543798a9-9c4b-435f-834a-74f20ae122b9'
 const meme1 = fs.readFileSync(`${__dirname}/data/meme_1.jpeg`)
 const meme2 = fs.readFileSync(`${__dirname}/data/meme_2.jpeg`)
 
+describe('GET /memes', () => {
+  it('should return all memes', async () => {
+    const res = await request(app).get('/memes')
+    expect(res.status).toBe(200)
+    expect(res.body.status).toBe('success')
+    expect(res.body.memes).toContain(MEME_1_ID)
+  })
+})
+
 describe('GET /meme/:id', () => {
   it('should return a meme', async () => {
     const res = await request(app).get(`/meme/${MEME_1_ID}`)
     const actual = Buffer.from(res.body, 'base64')
-    expect(res.status).toEqual(200)
+    expect(res.status).toBe(200)
     expect(actual).toEqual(meme1)
   })
 })
