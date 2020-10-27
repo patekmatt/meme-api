068e72c0131dc42e7042279b295afac15e5909ac
patekmatt <patekmatt@protonmail.com>
2020-10-26 19:21:00 -0500

    Not Found and Get All Memes (#5)
    * add morgan for logging
    
    * error handling
    
    * changes
    
    * get all memes endpoint
    
    * more changes

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
+const morgan = require('morgan')
 const { validate, ValidationError } = require('express-validation')
 require('dotenv').config()
 
 const {
-  addMeme, getMeme, randomMeme, removeMeme, updateMeme
+  addMeme, getAllMemes, getMeme, randomMeme, removeMeme, updateMeme
 } = require('./lib/service')
 const schemas = require('./lib/schemas')
 
 const app = express()
+app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' }))
 
 const port = 3000
 
 app.post('/meme', validate(schemas.addMeme), addMeme)
+app.get('/memes', getAllMemes)
 app.get('/meme/random', randomMeme)
 app.route('/meme/:id')
   .delete(validate(schemas.getMeme), removeMeme)
@@ -23,7 +26,10 @@ app.use((err, req, res, next) => {
   if (err instanceof ValidationError) {
     return res.status(err.statusCode).json(err)
   }
-  return res.status(500).json(err)
+  return res.status(500).json({
+    status: 'fail',
+    message: 'Internal Server Error'
+  })
 })
 
 app.listen(port, () => {
